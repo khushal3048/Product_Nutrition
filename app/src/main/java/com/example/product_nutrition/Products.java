3de62688a1d082c3d36b2808f7bc3444e865b4bf
1894170 <rajnishkaur585@gmail.com>
2019-07-10 11:54:10 -0400

    Product Class and list_item.xml file Prepared for infalte with ListView in MainActivity

--- app/src/main/java/com/example/product_nutrition/Products.java
+++ app/src/main/java/com/example/product_nutrition/Products.java
@@ -5,6 +5,8 @@ package com.example.product_nutrition;
  * Product Class used for display Product Data Into ListView
  * Data that we get from API
  * In this Class, There is only Getter and Setter Methods to Get data about product and Set Data of Product one by one
+ * Also this class is used for pass data from MainActivity to ProductDetails
+ * Where we display full Description of Single Product
  *
 */
 public class Products {
